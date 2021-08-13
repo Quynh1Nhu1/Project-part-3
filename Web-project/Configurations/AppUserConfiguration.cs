3fac6e81d81756ca39c80879166f8ebd215d29e3
thaibao0225 <thaibao0225@gmail.com>
2021-08-14 04:12:19 +0700

    Done Data Seeding

---
+++ Web-project/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Web_project.Entity;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUsers>
+    {
+        public void Configure(EntityTypeBuilder<AppUsers> builder)
+        {
+            builder.ToTable("AppUsers");
+            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.LastName).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Dob).IsRequired();
+
+        }
+    }
+}
---
+++ Web-project/Entity/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Web_project.Entity
+{
+    public class Category
+    {
+        public int Id { set; get; }
+        public int SortOrder { set; get; }
+
+        public string CategoryName { set; get; }
+
+        public string CategoryType { set; get; }
+
+        public string CategorySex { set; get; }
+
+        //public bool IsShowOnHome { set; get; }
+        //public int? ParentId { set; get; }
+        //public Status Status { set; get; }
+
+        public List<ProductInCategory> ProductInCategories { get; set; }
+
+        //public List<CategoryTranslation> CategoryTranslations { get; set; }
+
+    }
+}
