d00c8bd7a486c3c384296b5fd130db941b3d3d93
thaibao0225 <thaibao0225@gmail.com>
2021-08-14 00:51:50 +0700

    Add project files.

---
+++ Web-project/Configurations/UserrConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_project.Entity;
+
+namespace Web_project.Configurations
+{
+    public class UserrConfigurations : IEntityTypeConfiguration<Userr>
+    {
+
+
+        public void Configure(EntityTypeBuilder<Userr> builder)
+        {
+            builder.HasKey(t => new { t.UserId });
+
+          
+
+        }
+
+    }
+}
